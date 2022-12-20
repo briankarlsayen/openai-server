0fa04ff767596c8906980f61dad0eec0bdad6581
BrianSayen <blu3fire89@gmail.com>
2022-12-20 16:34:52 +0800

    update

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 app.use(express.json())
-app.use(cors())
+app.use(cors)
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });

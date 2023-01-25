eb4ae54a15a38627fe99ceb0244e083bfe9d1497
BrianSayen <blu3fire89@gmail.com>
2023-01-25 16:51:41 +0800

    update

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
-import cors from 'cors';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-app.post('/rephrase', async(req: Request, res: Response) => {
+app.post("/rephrase", async (req: Request, res: Response) => {
   const { prompt } = req.body;
-  const promptQuestion = `Rephrase using highly sophisticated language: ${prompt}`
+  const promptQuestion = `Rephrase using highly sophisticated language: ${prompt}`;
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-002",
@@ -26,19 +26,25 @@ app.post('/rephrase', async(req: Request, res: Response) => {
       frequency_penalty: 0,
       presence_penalty: 0,
     });
-    if(!response.data.choices[0].text) return res.status(422).json({ message: "Invalid response"});
+    if (!response.data.choices[0].text)
+      return res.status(422).json({ message: "Invalid response" });
     const responseText = response.data.choices[0].text;
-    const newText = responseText?.replace(/\n/g, '')
+    const newText = responseText?.replace(/\n/g, "");
 
-    res.status(200).json({data: newText})
+    res.status(200).json({ data: newText });
   } catch (err) {
-    console.log('error', err)
-    res.status(422).json(err)
+    console.log("error", err);
+    res.status(422).json(err);
   }
 });
 
-app.get('/', async(req: Request, res: Response) => {
-  res.status(200).json({message: "Alive alive", key: process.env.OPENAI_API_KEY ? true: false});
+app.get("/", async (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({
+      message: "Alive alive",
+      key: process.env.OPENAI_API_KEY ? true : false,
+    });
 });
 
 app.listen(port, () => {
